1e243c74d8d7835e08e3f3148ce73f74b7e32467
Rudra Dave <rudradave490@gmail.com>
2025-10-04 01:43:51 +0530

    Create project details and user profile UI

---
+++ PeerCraft/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PeerCraft.Models;
+using System.Collections.Generic;
+
+namespace PeerCraft.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public string Username { get; set; }
+        public IEnumerable<Project> OwnedProjects { get; set; }
+    }
+}
---
+++ PeerCraft/ViewModels/ProjectDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PeerCraft.Models;
+using System.Collections.Generic;
+
+namespace PeerCraft.ViewModels
+{
+    public class ProjectDetailViewModel
+    {
+        public Project Project { get; set; }
+        public IEnumerable<Technology> Technologies { get; set; }
+        public IEnumerable<User> TeamMembers { get; set; }
+        public IEnumerable<ProjectTask> Tasks { get; set; }
+        public double ProgressPercentage { get; set; }
+        public bool IsCurrentUserTeamMember { get; set; }
+        public AddTaskViewModel AddTaskForm { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}

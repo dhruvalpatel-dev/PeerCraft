f684a3dc246d23b59264bedb26e94931968776ba
Rudra Dave <rudradave490@gmail.com>
2025-10-04 01:47:24 +0530

    Implement project edit and delete functionality

---
+++ PeerCraft/ViewModels/EditProjectViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeerCraft.ViewModels
+{
+    public class EditProjectViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Display(Name = "GitHub Link (Optional)")]
+        [Url]
+        public string GitHubLink { get; set; }
+
+        public List<SelectableTechnologyViewModel> AvailableTechnologies { get; set; }
+    }
+}

b16abf1a0b0f3e72e6c2c88ac263b05377c88acc
Rudra Dave <rudradave490@gmail.com>
2025-10-04 01:42:20 +0530

    Add project creation UI and view models

---
+++ PeerCraft/ViewModels/CreateProjectViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeerCraft.ViewModels
+{
+    public class CreateProjectViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Display(Name = "GitHub Link (Optional)")]
+        [Url]
+        public string GitHubLink { get; set; }
+
+        [Display(Name = "Team Members (Optional)")]
+        [DataType(DataType.MultilineText)]
+        public string TeamMembers { get; set; }
+
+        public List<SelectableTechnologyViewModel> AvailableTechnologies { get; set; }
+    }
+}
---
+++ PeerCraft/ViewModels/SelectableTechnologyViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PeerCraft.ViewModels
+{
+    public class SelectableTechnologyViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}

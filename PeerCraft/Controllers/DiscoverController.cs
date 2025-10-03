c0a735747e1a36419a0c95c0673076105a3f8947
Rudra Dave <rudradave490@gmail.com>
2025-10-04 01:44:23 +0530

    Add Discover page with filtering

---
+++ PeerCraft/Controllers/DiscoverController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeerCraft.Data;
+using PeerCraft.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeerCraft.Controllers
+{
+    [Authorize]
+    public class DiscoverController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DiscoverController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Discover
+        public async Task<IActionResult> Index(int[] selectedTechnologies)
+        {
+            // Base query to get all projects with their related data
+            var projectsQuery = _context.Projects
+                .Include(p => p.Owner)
+                .Include(p => p.ProjectTechnologies).ThenInclude(pt => pt.Technology)
+                .AsQueryable();
+
+            // If any technology filters are selected, apply them
+            if (selectedTechnologies != null && selectedTechnologies.Any())
+            {
+                // Filter projects to include only those that have at least one of the selected technologies
+                projectsQuery = projectsQuery.Where(p => p.ProjectTechnologies.Any(pt => selectedTechnologies.Contains(pt.TechnologyId)));
+            }
+
+            // Fetch all available technologies to display as filter options
+            var allTechnologies = await _context.Technologies.OrderBy(t => t.Name).ToListAsync();
+
+            var viewModel = new DiscoverViewModel
+            {
+                Projects = await projectsQuery.OrderByDescending(p => p.Id).ToListAsync(),
+                AvailableTechnologies = allTechnologies.Select(t => new SelectableTechnologyViewModel
+                {
+                    Id = t.Id,
+                    Name = t.Name,
+                    // Mark as selected if its ID is in the submitted filter array
+                    IsSelected = selectedTechnologies != null && selectedTechnologies.Contains(t.Id)
+                }).ToList(),
+                SelectedTechnologies = selectedTechnologies
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ PeerCraft/ViewModels/DiscoverViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PeerCraft.Models;
+using System.Collections.Generic;
+
+namespace PeerCraft.ViewModels
+{
+    public class DiscoverViewModel
+    {
+        public IEnumerable<Project> Projects { get; set; }
+        public List<SelectableTechnologyViewModel> AvailableTechnologies { get; set; }
+        public int[] SelectedTechnologies { get; set; }
+    }
+}
